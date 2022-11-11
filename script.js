a8431108964f4848232854f0eb921317cdece1e7
Shayan Saeed <62258613+IbnArabi@users.noreply.github.com>
2022-11-11 11:49:09 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 //window.alert('Welcome to KSPP, King Salman Pilot Projects by the Custodian of The Two Holy Mosques King Salman bin AbdulAziz and the Crown Prince His Highness Muhammad Bin Salaman Bin AbdulAziz. This is developed to help the Pilots of the airlines in the Kingdom and the Middle East to arrange their flight plans and work. We also thank the respected rulers of United Arab Emirates, Bahrain, Oman, Yemen, and Jordan. </> with thanks by Shayan LLC \n');
 // translate in arabic
 
+function newbid(){
+    alert("Welcome to KSPP\n Our new improved Bidding system makes it easier to bid flights and read information.");
+}
+
+function lonwz(){
+    alert("Landing On War Zones\nLanding where any type of military force is operational is not permitted except with permission. Please request for permission on the respective air controllers.");
+}
+
+function sup(){
+    alert("Support\nFor Support you can always contact the provided services at:\n pilotsupport@kspp.gov.sa")
+}
