babef8264ea1ba4d40f5d29fbf2c82ee823018ce
Shayan Saeed <62258613+IbnArabi@users.noreply.github.com>
2022-12-18 12:57:11 +0000

    Fixed Bugs

--- viewbid.js
+++ viewbid.js
@@ -4,7 +4,7 @@ var allBids = JSON.parse(localStorage.getItem('bids')) || [];
 document.getElementById("bid-container").innerHTML = allBids.map((bid, index) => {
     return `
         <div class="bid">
-            <div class="flight-info"><span id="identifier">${icr}</span><span class="flight-no flight time">${bid.flight}</span></div>
+            <div class="flight-info"><span class="flight-no flight time">${bid.flight}</span></div>
             <div class="bid-amount"><span class="title ">Departure: </span><span class="time">${bid.amount}</span></div>
             <button class="btn btn-primary hahah" id="${bid.id}">Full Info</button>
         </div>
@@ -42,21 +42,4 @@ buttons.forEach(function(button) {
 function veiwFullBid(e) {
     var bidId = e;
     console.log(bidId);
-}
-
-/*
-<div class="bid-route"><span class="title">Route: </span>${bid.route}</div>
-            <div class="bid-aircraft"><span class="title">Aircraft Type: </span>${bid.aircraft}</div>
-            <div class="bid-reg"><span class="title">Registration: </span>${bid.reg}</div>
-            <div class="bid-dep"><span class="title">Departure ICAO:</span>${bid.dep}</div>
-            <div class="bid-arr"><span class="title">Arrival ICAO: </span>${bid.arr}</div>
-*/
-
-//adding flight icr
-if(airline === "Saudia"){
-    icr = "SVA";
-}
-
-else{
-    //do nothing
 }
